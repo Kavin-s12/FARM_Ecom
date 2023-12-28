52dbef9f9a4a372930334910970589980fc9b783
kavin-s12 <kavinswork@gmail.com>
2023-12-28 15:42:11 +0530

    change in path of build

--- backend/server.js
+++ backend/server.js
@@ -30,7 +30,7 @@ if (process.env.NODE_ENV == "production") {
   app.use(express.static(path.join(_dirname, "../frontend/build")));
 
   app.get("*", (req, res) =>
-    res.sendFile(path.resolve(_dirname, "frontend", "build", "index.html"))
+    res.sendFile(path.resolve(_dirname, "../frontend", "build", "index.html"))
   );
 }
 
