b158eb9e531668ac43fec211be755aa8615df26d
kavin-s12 <kavinswork@gmail.com>
2023-12-28 19:51:13 +0530

    update in redis port

--- backend/redisClient.js
+++ backend/redisClient.js
@@ -1,15 +1,22 @@
 import redis from "redis";
+import dotenv from "dotenv";
 
-const REDISPORT = process.env.REDISPORT || 6379;
+dotenv.config();
 
-const redisClient = redis.createClient(REDISPORT);
+const REDIS_HOST = process.env.REDIS_HOST || "localhost";
+const REDIS_PORT = process.env.REDIS_PORT || 6379;
+
+const redisClient = redis.createClient({
+  host: REDIS_HOST,
+  port: REDIS_PORT,
+});
 
 redisClient.on("error", (err) => {
   console.error(`Redis client error: ${err}`);
 });
 
 redisClient.on("ready", () => {
-  console.log("Redis is ready");
+  console.log(`Redis is ready : ${REDIS_PORT}`);
 });
 
 redisClient.on("end", () => {
